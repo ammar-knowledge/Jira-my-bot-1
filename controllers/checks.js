b00322929f71a8b805ca7a1710c2ba8f2fd3e522
Ammar Ahmed Butt <ammar93butt@gmail.com>
2023-05-16 02:21:17 +0500

    Update checks.js

--- controllers/checks.js
+++ controllers/checks.js
@@ -5,7 +5,7 @@ module.exports.listForSuite = async (app, context) => {
     const owner = context.payload.repository.owner.login
     const repository = context.payload.repository.name
     const checkSuiteId = context.payload.check_suite.id
-    const listOfCheckRuns = await listForSuite(context, owner, repository, checkSuiteId)
+    const listOfCheckRuns = await listForSuite(context, owner||user, repository, checkSuiteId)
     return listOfCheckRuns
   } catch (error) {
     app.log(error)
