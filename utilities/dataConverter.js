ee3f33bb659152e89d5e9539857b666ea49aa503
Ammar Ahmed Butt <ammar.butt@atlassiancommunity.com>
2023-06-01 11:03:16 +0500

    add

--- utilities/dataConverter.js
+++ utilities/dataConverter.js
@@ -4,9 +4,10 @@
  */
 module.exports.base64ToString = (data) => {
   try {
-    let buff = new Buffer(data, 'base64')
+    let buff = Buffer.alloc(data, 'base64')
     return buff.toString()
-  } catch (error) {
+      } catch (error) 
+  {
     return error
   }
 }
